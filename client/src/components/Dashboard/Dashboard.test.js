0c89899f794c3ddc97eb20e6e44b1803d5941ea0
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-14 17:01:25 -0400

    Completes tests for rendering Dashboard buttons

--- client/src/components/Dashboard/Dashboard.test.js
+++ client/src/components/Dashboard/Dashboard.test.js
@@ -7,4 +7,24 @@ describe('<Dashboard />', () => {
   it('should render', () => {
     render(<Dashboard />);
   });
+
+  it("should render a 'ball' button", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/ball/i);
+  })
+
+  it("should render a 'strike' button", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/strike/i);
+  })
+
+  it("should render a 'foul' button", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/foul/i);
+  })
+
+  it("should render a 'hit' button", () => {
+    const { getByText } = render(<Dashboard />);
+    getByText(/hit/i);
+  })
 })
